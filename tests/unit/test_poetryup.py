2eaa517d9a21f0a88e17c8534534aef454313c46
Mousa Zeid Baker <mzeb@moodagent.com>
2021-09-13 19:19:41 +0200

    update tests

--- tests/unit/test_poetryup.py
+++ tests/unit/test_poetryup.py
@@ -1,10 +1,26 @@
-import toml
+import os
+from pathlib import Path
+
 from poetryup.poetryup import poetryup
 
 
-def test_poetryup_should_bump_version(pyproject_fixture, mock_poetry_commands) -> None:
-    updated_pyproject_str = poetryup(pyproject_fixture)
-    updated_pyproject_dict = toml.loads(updated_pyproject_str)
-    actual_version = updated_pyproject_dict["tool"]["poetry"]["dependencies"]["poetryup"]
-    expected_version = "^0.2.0"
-    assert(actual_version == expected_version)
+def pytest_generate_tests(metafunc) -> None:
+    input_pyproject_path = os.path.join(os.path.dirname(__file__), "fixtures/input_pyproject")
+    input_pyprojects = [file.read_text() for file in Path(input_pyproject_path).glob("*.toml")]
+
+    expected_pyproject_path = os.path.join(os.path.dirname(__file__), "fixtures/expected_pyproject")
+    expected_pyprojects = [file.read_text() for file in Path(expected_pyproject_path).glob("*.toml")]
+
+    argvalues = list(zip(input_pyprojects, expected_pyprojects))
+    ids = [file.name for file in Path(input_pyproject_path).glob("*.toml")]
+
+    metafunc.parametrize(
+        argnames=("input_pyproject", "expected_pyproject"),
+        argvalues=argvalues,
+        ids=ids
+    )
+
+
+def test_poetryup(input_pyproject: str, expected_pyproject: str, mock_poetry_commands) -> None:
+    updated_pyproject = poetryup(input_pyproject)
+    assert(updated_pyproject == expected_pyproject)

97780d649488c64a9d445c92312b035350653efe
Mousa Zeid Baker <mzeb@moodagent.com>
2021-09-13 19:42:00 +0200

    change single quotes to double quotes

--- poetryup/poetryup.py
+++ poetryup/poetryup.py
@@ -20,7 +20,7 @@ def _run_poetry_update() -> None:
     """Run poetry update command
     """
 
-    subprocess.run(['poetry', 'update'])
+    subprocess.run(["poetry", "update"])
 
 
 def _run_poetry_show() -> str:
@@ -31,7 +31,7 @@ def _run_poetry_show() -> str:
     """
 
     return subprocess.run(
-        ['poetry', 'show', '--tree'],
+        ["poetry", "show", "--tree"],
         capture_output=True
     ).stdout.decode()
 
@@ -46,8 +46,8 @@ def _list_dependencies() -> List[Dependency]:
     output = _run_poetry_show()
 
     dependencies: List[Dependency] = []
-    for line in output.split('\n'):
-        if re.match('^[a-zA-Z]+', line) is not None:
+    for line in output.split("\n"):
+        if re.match("^[a-zA-Z]+", line) is not None:
             name, version, *_ = line.split()
             dependency = Dependency(name=name, version=version)
             dependencies.append(dependency)
@@ -68,14 +68,14 @@ def _bump_versions_in_pyproject(dependencies: List[Dependency], pyproject: Dict)
 
     for dependency in dependencies:
         value = utils.lookup_nested_dict(
-            dictionary=pyproject['tool']['poetry'],
+            dictionary=pyproject["tool"]["poetry"],
             key=dependency.name
         )
 
-        if value.startswith(('^', '~')):
+        if value.startswith(("^", "~")):
             new_version = value[0] + dependency.version
             utils.update_nested_dict(
-                dictionary=pyproject['tool']['poetry'],
+                dictionary=pyproject["tool"]["poetry"],
                 key=dependency.name,
                 new_value=new_version
             )
@@ -98,9 +98,9 @@ def poetryup(pyproject_str: str) -> str:
     updated_pyproject_dict = _bump_versions_in_pyproject(dependencies, pyproject_dict)
 
     # in order to preserve the order of the pyproject file, append build-system to the end
-    build_system = {'build-system': updated_pyproject_dict.pop('build-system')}
+    build_system = {"build-system": updated_pyproject_dict.pop("build-system")}
     updated_pyproject_str = toml.dumps(updated_pyproject_dict)
-    updated_pyproject_str += '\n' + toml.dumps(build_system)
+    updated_pyproject_str += "\n" + toml.dumps(build_system)
 
     return updated_pyproject_str
 
@@ -108,12 +108,12 @@ def poetryup(pyproject_str: str) -> str:
 def main():
     # read pyproject.toml file
     try:
-        pyproject_str = Path('pyproject.toml').read_text()
+        pyproject_str = Path("pyproject.toml").read_text()
     except FileNotFoundError:
-        raise Exception('poetryup could not find a pyproject.toml file in current directory')
+        raise Exception("poetryup could not find a pyproject.toml file in current directory")
 
     updated_pyproject_str = poetryup(pyproject_str)
-    Path('pyproject.toml').write_text(updated_pyproject_str)
+    Path("pyproject.toml").write_text(updated_pyproject_str)
 
 
 if __name__ == "__main__":

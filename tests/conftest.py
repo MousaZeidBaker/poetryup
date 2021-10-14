f39b5b0491ede04fb56ee67a9cf245ec50372746
Mousa Zeid Baker <mzeb@moodagent.com>
2021-10-15 00:11:05 +0200

    format with black and isort

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,11 @@ from pytest_mock import MockerFixture
 def mock_poetry_commands(mocker: MockerFixture) -> None:
     mocker.patch("poetryup.poetryup._run_poetry_update", return_value=None)
 
-    return_value = "poetryup 0.2.0 Run poetry update and bump versions in pyproject.toml file" \
-                   "\n└── toml >=0.10.2,<0.11.0"
-    mocker.patch("poetryup.poetryup._run_poetry_show", return_value=return_value)
+    return_value = (
+        "poetryup 0.2.0 Update dependencies and bump their version in the "
+        "pyproject.toml file"
+        "\n└── toml >=0.10.2,<0.11.0"
+    )
+    mocker.patch(
+        "poetryup.poetryup._run_poetry_show", return_value=return_value
+    )
--- tests/unit/test_poetryup.py
+++ tests/unit/test_poetryup.py
@@ -5,11 +5,20 @@ from poetryup.poetryup import poetryup
 
 
 def pytest_generate_tests(metafunc) -> None:
-    input_pyproject_path = os.path.join(os.path.dirname(__file__), "fixtures/input_pyproject")
-    input_pyprojects = [file.read_text() for file in Path(input_pyproject_path).glob("*.toml")]
+    input_pyproject_path = os.path.join(
+        os.path.dirname(__file__), "fixtures/input_pyproject"
+    )
+    input_pyprojects = [
+        file.read_text() for file in Path(input_pyproject_path).glob("*.toml")
+    ]
 
-    expected_pyproject_path = os.path.join(os.path.dirname(__file__), "fixtures/expected_pyproject")
-    expected_pyprojects = [file.read_text() for file in Path(expected_pyproject_path).glob("*.toml")]
+    expected_pyproject_path = os.path.join(
+        os.path.dirname(__file__), "fixtures/expected_pyproject"
+    )
+    expected_pyprojects = [
+        file.read_text()
+        for file in Path(expected_pyproject_path).glob("*.toml")
+    ]
 
     argvalues = list(zip(input_pyprojects, expected_pyprojects))
     ids = [file.name for file in Path(input_pyproject_path).glob("*.toml")]
@@ -17,10 +26,12 @@ def pytest_generate_tests(metafunc) -> None:
     metafunc.parametrize(
         argnames=("input_pyproject", "expected_pyproject"),
         argvalues=argvalues,
-        ids=ids
+        ids=ids,
     )
 
 
-def test_poetryup(input_pyproject: str, expected_pyproject: str, mock_poetry_commands) -> None:
+def test_poetryup(
+    input_pyproject: str, expected_pyproject: str, mock_poetry_commands
+) -> None:
     updated_pyproject = poetryup(input_pyproject)
-    assert(updated_pyproject == expected_pyproject)
+    assert updated_pyproject == expected_pyproject

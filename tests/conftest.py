19ea300dd0772403e5b5eec3748325d2106e6f2c
Mousa Zeid Baker <mzeb@moodagent.com>
2021-10-25 07:34:47 +0200

    rename entrypoint file from poetryup.py to main.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from pytest_mock import MockerFixture
 
 @pytest.fixture(scope="function")
 def mock_poetry_commands(mocker: MockerFixture) -> None:
-    mocker.patch("poetryup.poetryup._run_poetry_update", return_value=None)
+    mocker.patch("poetryup.main._run_poetry_update", return_value=None)
 
     return_value = (
         "poetryup 0.2.0 Update dependencies and bump their version in the "
@@ -12,5 +12,5 @@ def mock_poetry_commands(mocker: MockerFixture) -> None:
         "\n└── toml >=0.10.2,<0.11.0"
     )
     mocker.patch(
-        "poetryup.poetryup._run_poetry_show", return_value=return_value
+        "poetryup.main._run_poetry_show", return_value=return_value
     )
